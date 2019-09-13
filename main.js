1bba6dc80a2dff309c0ba635f6d689e3ab017101
Albin Esteban Frias Rojas <afrias@cam.com>
2019-09-13 15:09:30 -0400

    Added add page

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let root = document.getElementById("root");
 
+function agregarCliente() {
+    var nombre = document.getElementById("Nombre").nodeValue;
+}
 
 function createTable() {
     let table = document.createElement('table');
@@ -9,4 +12,17 @@ function createTable() {
     let colum3 = document.createElement('td');
     let colum4 = document.createElement('td');
     let colum5 = document.createElement('td');
+    colum1.textContent = "Codigo";
+    colum2.textContent = "Nombre";
+    colum3.textContent = "Balance";
+    colum4.textContent = "Fecha Registro";
+    colum5.textContent = "Opciones";
+    mainRow.appendChild(colum1);
+    mainRow.appendChild(colum2);
+    mainRow.appendChild(colum3);
+    mainRow.appendChild(colum4);
+    mainRow.appendChild(colum5);
+    table.appendChild(mainRow);
+    root.appendChild(table);
 }
+
